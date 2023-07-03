a8becfa011ddc0ce8adaf387665be96c48da5edc
Arminias <arminias@t-online.de>
2023-07-03 10:16:44 +0200

    Apparently Coerce does not work here in release & archive name fix

--- src/main/java/btw/community/arminias/metadata/mixin/AnvilChunkLoaderMixin.java
+++ src/main/java/btw/community/arminias/metadata/mixin/AnvilChunkLoaderMixin.java
@@ -6,7 +6,6 @@ import btw.community.arminias.metadata.extension.NBTTagCompoundExtension;
 import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -26,7 +25,6 @@ public class AnvilChunkLoaderMixin {
     }
 
 
-    @Coerce
     @Inject(method = "readChunkFromNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/ExtendedBlockStorage;setBlocklightArray(Lnet/minecraft/src/NibbleArray;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT, require = 0)
     //private void readChunkFromNBT(World par1World, NBTTagCompound par2NBTTagCompound, CallbackInfoReturnable<Chunk> cir, int var3, int var4, Chunk var5, NBTTagList var6, int var7, ExtendedBlockStorage[] var8, int var9, int var10, NBTTagCompound var11, int var12, ExtendedBlockStorage var13) {
     private void readChunkFromNBT(World par1World, NBTTagCompound par2NBTTagCompound, CallbackInfoReturnable<Chunk> cir, int var3, int var4, Chunk var5, NBTTagList var6, byte var7, ExtendedBlockStorage[] var8, boolean var9, int var10, NBTTagCompound var11, byte var12, ExtendedBlockStorage var13) {
@@ -34,8 +32,8 @@ public class AnvilChunkLoaderMixin {
     }
 
 
-    /*@Inject(method = "readChunkFromNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/ExtendedBlockStorage;setBlocklightArray(Lnet/minecraft/src/NibbleArray;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT, require = 0)
+    @Inject(method = "readChunkFromNBT", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/ExtendedBlockStorage;setBlocklightArray(Lnet/minecraft/src/NibbleArray;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT, require = 0)
     private void readChunkFromNBT(World par1World, NBTTagCompound par2NBTTagCompound, CallbackInfoReturnable<Chunk> cir, int var3, int var4, Chunk var5, NBTTagList var6, int var7, ExtendedBlockStorage[] var8, int var9, int var10, NBTTagCompound var11, int var12, ExtendedBlockStorage var13) {
         ((ExtendedBlockStorageExtension) var13).setBlockExtraMetadataArray(new HunkArray(((NBTTagCompoundExtension) var11).getLongArray("ExtraData"), 4));
-    }*/
+    }
 }
